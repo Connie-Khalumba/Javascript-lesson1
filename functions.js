7d6def6665b90d56722b5574c5bb9f5a8c89a45e
constance khalumba <constancekhalumba@gmail.com>
2024-07-08 11:07:58 +0300

    added some pages and functions

---
+++ functions.js
@@ -0,0 +1,43 @@
+// function displayName() {
+//     name = "John Doe";
+// 
+//     return name;
+// }
+
+// console.log(displayName());
+
+function displaySum() {
+    var a = 10;
+    var b = 20;
+
+  return (sum = a + b);
+}
+
+console.log(displaySum());
+
+function displayProduct() {
+  var num1 = 30;
+  var num2= 20;
+
+  return (product = num1 * num2)
+}
+
+console.log(displayProduct());
+
+function displayProduct() {
+  var num1 = 30;
+  var num2= 20;
+
+  return (product = num1 * num2)
+}
+
+console.log(displayProduct());
+
+function displadisplayProduct() {
+  var num1 = 300;
+  var num2= 20;
+
+  return (product = num1 / num2)
+}
+
+console.log(displayProduct());
---
+++ variables.js
@@ -0,0 +1,11 @@
+namesInClass = ["Anthony", "Constance", 21, true, null, "Jane","Kate"]
+
+erastusAge = 23;
+
+isKenyan = true;
+
+console.log(typeof namesInClass);
+
+console.log(typeof erastusAge);
+
+console.log(typeof isKenyan);
